3440844e538dc6148e2d8974b2d47dcdf74a5214
Kleyverson <kleyverson@pop-os.localdomain>
2022-03-02 03:51:02 -0300

    Segundo commit

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = { mongoURI: "mongodb+srv://Kleyverson:Kymovel1800@sistemadeacesso.ry9fv.mongodb.net/sistemaDeAcesso?retryWrites=true&w=majority" }
+    module.exports = { mongoURI: "mongodb+srv://Kleyverson:Kymovel19@projetointegrador.iagn4.mongodb.net/projetoIntegrador?retryWrites=true&w=majority" }
 }else{
     module.exports = { mongoURI: "mongodb://localhost/projeto_integrador" }
 }
