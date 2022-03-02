b83e9998b2423cd7dbc150bc8f0640651f501b20
Kleyverson <kleyverson@pop-os.localdomain>
2022-03-02 14:01:08 -0300

    Ajuste na conexao com o Mongo

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = { mongoURI: "mongodb+srv://Kleyverson:Kymovel19@projetointegrador.iagn4.mongodb.net/projetoIntegrador?retryWrites=true&w=majority" }
+    module.exports = { mongoURI: "mongodb+srv://Kleyverson:4c7SjQLgK4lY1zSC@projetointegrador.iagn4.mongodb.net/projetoIntegrador?retryWrites=true&w=majority" }
 }else{
     module.exports = { mongoURI: "mongodb://localhost/projeto_integrador" }
 }
